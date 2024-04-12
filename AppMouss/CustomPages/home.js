5e9c45bf816093c5badb8c8f0d7ede85ac5ebe09
mchanfiou <166795272+mchanfiou@users.noreply.github.com>
2024-04-12 11:27:39 +0200

    4e push

--- AppMouss/CustomPages/home.js
+++ AppMouss/CustomPages/home.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { ImageBackground ,StyleSheet, Text, View, Button } from 'react-native';
 
+const image = {uri:'./Images/background.jpg'};
 function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Bienvenue!</Text>
-      <StatusBar style="auto" />
-      <Button title="Rechercher" onPress={() => navigation.navigate('Rechercher')}/>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <Text>Bienvenue!</Text>
+            <StatusBar style="auto" />
+            <Button title="Rechercher" onPress={() => navigation.navigate('Rechercher')}/>
+        </ImageBackground>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,  
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
 });
